e4fbc7bd5391051686a03a9679d368f6d3f014f9
Daniel Eades <danieleades@hotmail.com>
2022-09-13 16:42:03 +0100

    fixup

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -6,7 +6,17 @@ from sphinx_graph.directives.vertex.directive import parse_parents
 from sphinx_graph.directives.vertex.info import Link
 
 
-@pytest.mark.parametrize("input,expected", [("REQ-01", [Link("REQ-01", None)])])
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ("REQ-01", [Link("REQ-01", None)]),
+        ("REQ-01:1234", [Link("REQ-01", "1234")]),
+        (
+            "REQ-01:1234, REQ-02,REQ-03:5678",
+            [Link("REQ-01", "1234"), Link("REQ-02", None), Link("REQ-03", "5678")],
+        ),
+    ],
+)
 def test_parse_parents(input: str, expected: list[Link]) -> None:
     output = parse_parents(input)
     assert output == expected
