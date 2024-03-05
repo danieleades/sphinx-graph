0ac39f046f68c219a0fefaba09cd08a46c8a2a2c
Daniel Eades <danieleades@hotmail.com>
2024-03-05 18:49:23 +0000

    don't allow duplicate table IDs

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,6 +1,11 @@
+from uuid import uuid4
+
 import pytest
 from sphinx.application import Sphinx
 from sphinx.errors import ConfigError
+from sphinx_graph.table.info import Info
+from sphinx_graph.table.state import State
+from sphinx_graph.vertex.state import DuplicateIdError
 
 
 @pytest.mark.sphinx(testroot="table")
@@ -23,3 +28,17 @@ def test_query_unknown_fails(app: Sphinx) -> None:
         match="no query registered with name 'unknown'",
     ):
         app.build()
+
+
+def test_duplicate_tables_not_allowed() -> None:
+    state = State(tables={})
+    uuid = uuid4()
+
+    state.insert(uuid, info=Info(docname="docname", query=None, args={}))
+
+    with pytest.raises(
+        DuplicateIdError,
+        match="test",
+    ):
+        # try to insert an existing uuid
+        state.insert(uuid, info=Info(docname="docname", query=None, args={}))
