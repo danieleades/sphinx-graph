d985e556d86b0378186168477cc8b6ba37215082
Daniel Eades <danieleades@hotmail.com>
2024-07-28 14:11:43 +0100

    fixup

--- docs/query/__init__.py
+++ docs/query/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 from sphinx_graph.vertex.query import query_wrapper
 
--- src/sphinx_graph/vertex/query.py
+++ src/sphinx_graph/vertex/query.py
@@ -2,11 +2,14 @@
 
 from __future__ import annotations
 
-from collections.abc import Callable, Iterable
 from functools import wraps
-from typing import Any, Concatenate, ParamSpec, Protocol
+from typing import TYPE_CHECKING, Any, ParamSpec, Protocol
 
-from sphinx_graph.vertex.state import State
+if TYPE_CHECKING:
+    from collections.abc import Callable, Iterable
+    from typing import Concatenate
+
+    from sphinx_graph.vertex.state import State
 
 P = ParamSpec("P")
 
@@ -20,13 +23,14 @@ Query = QueryBase[Any]
 
 def query_wrapper(func: Callable[Concatenate[State, P], Iterable[str]]) -> Query:
     @wraps(func)
-    def wrapper(state: State, **kwargs: Any) -> Iterable[str]:
+    def wrapper(state: State, **kwargs: P.kwargs) -> Iterable[str]:
         return func(state, **kwargs)
 
     return wrapper
 
 
-def noop(state: State, **_kwargs: Any) -> Iterable[str]:
+@query_wrapper
+def noop(state: State) -> Iterable[str]:
     """Simply returns all vertices, in the same order as they arrive."""
     return state.vertices.keys()
 
