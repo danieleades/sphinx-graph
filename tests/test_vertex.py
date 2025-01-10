4d673e1fd316505f9b1f4ebf809c3ba2c91b9bf1
Daniel Eades <danieleades@hotmail.com>
2025-01-10 21:57:57 +0000

    chore(ci): update ruff version

--- tests/test_vertex.py
+++ tests/test_vertex.py
@@ -78,6 +78,6 @@ def test_invalid_parent(app: Sphinx) -> None:
     app.warningiserror = True
     with pytest.raises(
         SphinxError,
-        match="^vertex '02' has a parent link to '03', but '03' doesn't exist$",
+        match=r"^vertex '02' has a parent link to '03', but '03' doesn't exist$",
     ):
         app.build()
