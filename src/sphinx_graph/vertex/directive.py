03ba0da6433c01b986357b07e933cc0c26c833ba
danieleades <33452915+danieleades@users.noreply.github.com>
2026-01-07 08:41:44 +0000

    chore(deps): bump pre-commit versions (#280)

--- src/sphinx_graph/vertex/directive.py
+++ src/sphinx_graph/vertex/directive.py
@@ -61,7 +61,7 @@ class Directive(SphinxDirective):
         if vertex_config.regex and not vertex_config.regex.match(uid):
             msg = (
                 f"vertex '{uid}' doesn't satisfy the configured regex"
-                f" ('{vertex_config.regex.pattern}')",
+                f" ('{vertex_config.regex.pattern}')"
             )
             logger.error(
                 msg,
@@ -70,9 +70,7 @@ class Directive(SphinxDirective):
             raise SphinxError(msg)
 
         if vertex_config.require_parent and len(parents) < 1:
-            msg = (
-                f"vertex '{uid}' is required to have at least one parent but has none",
-            )
+            msg = f"vertex '{uid}' is required to have at least one parent but has none"
             logger.error(
                 msg,
                 location=(self.env.docname, self.lineno),
@@ -129,7 +127,8 @@ class Directive(SphinxDirective):
         3. directive configuration (local config set on the directive)
         """
         return (
-            self._default_config()
+            self
+            ._default_config()
             .override(self._type_config())
             .override(self._directive_config())
         )
